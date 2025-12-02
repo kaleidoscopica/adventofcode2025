c72d658f1d470b2dced106cf7f9ff6bb8e20fa54
kaleidoscopica <rayearth53@yahoo.com>
2025-12-02 11:19:18 -0600

    Day 1-1 complete

---
+++ Day1/1-1.py
@@ -0,0 +1,24 @@
+def main():
+
+  rotations = []
+  
+  with open('input.txt') as file:
+    rotations = [line.strip() for line in file]
+
+  pos = 50
+  times_at_zero = 0
+  
+  for rotation in rotations:
+    direction = rotation[0]
+    distance = int(rotation[1:])
+    if direction == 'R':
+      pos = (pos + distance) % 100
+    elif direction == 'L':
+      pos = (pos - distance) % 100
+    if pos == 0:
+      times_at_zero += 1
+    
+  print('The number of times the combination lock hit zero is:', times_at_zero)
+
+
+main()
